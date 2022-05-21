4ba3fe043e4fcda1dea295e02262fd0c66130fe2
jakuSk <jakub471@gmail.com>
2022-05-21 23:58:03 +0200

    whitelist script done

---
+++ src/services/whitelist_script/config_service.py
@@ -0,0 +1,28 @@
+import os
+ import json
+
+
+ class ConfigService:
+     def __init__(self, config_name, ):
+         self.__config = self.__load_config(config_name)
+
+     def __load_config(self, config_name) -> dict:
+         with open(config_name, 'r') as f:
+             config = json.load(f)
+             return config
+
+     def __get_config_member(self, key_path: str):
+         """Finds given element by key_path in __config after spliting the key_path for each eleme"""
+         split_path = key_path.split(':')
+         temp_config = self.__config
+
+         for key in split_path:
+             temp_config = temp_config.get(key)
+
+         return temp_config
+
+     def get_value(self, property_path: str):
+         """Returns value from configuration file. Use membered path for specification - i.e. Logs.Path > return Path value inside Logs object"""
+
+         value = self.__get_config_member(property_path)
+         return value
---
+++ src/services/whitelist_script/whitelist.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+ import getopt
+ from getopt import GetoptError
+ import sys
+ import os
+ from curses import meta
+ import sqlalchemy as db
+
+ from config_service import ConfigService
+
+ def main(argv):
+     try:
+       opts, args = getopt.getopt(argv,"h:u:",["username="])
+     except getopt.GetoptError:
+         print('whitelist.py -u <username>')
+         sys.exit(2)
+     for opt, arg in opts:
+         if opt == '-h':
+             print('whitelist.py -u <username>')
+             sys.exit()
+         elif opt in ("-u", "--username"):
+             username = arg
+
+     config_service = ConfigService('config.json')
+     db_user = config_service.get_value('database:user')
+     connection_string = config_service.get_value(
+         'database:connection_string')
+
+     connection_string = connection_string.replace('{db_user}', db_user)
+     connection_string = connection_string.replace(
+         '{db_pass}', os.environ['db_pass'])
+
+     engine = db.create_engine(connection_string)
+     connection = engine.connect()
+     metadata = db.MetaData()
+     users_table = db.Table('users_identity', metadata, autoload=True, autoload_with=engine)
+
+     user_label_query = f"""SELECT rc.username, ui.umbrella_label
+ FROM radcheck rc
+ JOIN users_identity ui
+ ON ui.radcheck_id = rc.id
+ WHERE rc.username = '{username}';
+ """
+
+     user_label_query_result = connection.execute(user_label_query).fetchall()
+     umbrella_label = user_label_query_result[0]['umbrella_label']
+
+     user_whitelist_query = f"""UPDATE users_identity ui
+ SET whitelisted_from = CURRENT_DATE
+ WHERE ui.umbrella_label = '{umbrella_label}';"""
+
+     connection.execute(user_whitelist_query)
+
+     print(f'User {username} whitelisted')
+
+ if __name__ == "__main__":
+    main(sys.argv[1:])
